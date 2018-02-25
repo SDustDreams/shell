8ef307018858620ad0e7675e08ebbcf7f0bf76cd
S_ChenM <1071333101@qq.com>
2018-02-25 23:11:10 +0800

    Update cm.sh

--- cm.sh
+++ cm.sh
@@ -6,23 +6,23 @@ echo "
 ---------------------------------------------------------
 【1】SSR安装
 
-【3】BBR算法加速+最新内核
+【2】BBR算法加速+最新内核
 
-【4】aria2后端搭建
+【3】aria2后端搭建
 
-【5】centos更换内核（锐速）
+【4】centos更换内核（锐速）
 
-【6】锐速安装程序
+【5】锐速安装程序
 
-【7】锐速卸载程序
+【6】锐速卸载程序
 
-【8】多端口开放
+【7】多端口开放
 
-【9】caddy安装
+【8】caddy安装
 
-【10】一键安装ariaNG和filemanger
+【9】一键安装ariaNG和filemanger
 
-【11】一键安装libsodium
+【10】一键安装libsodium
 ---------------------------------------------------------
 "
 read os
@@ -41,7 +41,7 @@ bash ssr.sh;
 fi
 
 #BBR算法加速
-if test $os == 3 ;then
+if test $os == 2 ;then
 echo '******正在下载BBR算法加速，请等待几分钟******';
 wget -N --no-check-certificate ${url}/bbr.sh;
 chmod +x bbr.sh;
@@ -51,7 +51,7 @@ bash bbr.sh;
 fi
 
 #aria2后端安装
-if test $os == 4 ;then
+if test $os == 3 ;then
 echo '******正在下载aria2后端，请等待几分钟******';
 wget -N --no-check-certificate ${url}/aria2.sh;
 chmod +x aria2.sh;
@@ -61,7 +61,7 @@ bash aria2.sh;
 fi
 
 #centos更换内核
-if test $os == 5 ;then
+if test $os == 4 ;then
 echo '******正在下载centos内核更换，请等待几分钟******';
 wget -N --no-check-certificate ${url}/ruisu.sh;
 echo -e '下载完成：			  [\033[32m  OK  \033[0m]';
@@ -70,7 +70,7 @@ bash ruisu.sh;
 fi
 
 #锐速安装程序
-if test $os == 6 ;then
+if test $os == 5 ;then
 echo '******正在下载锐速程序，请等待几分钟******';
 wget -N --no-check-certificate ${url}/serverspeeder.sh;
 echo -e '下载完成：			  [\033[32m  OK  \033[0m]';
@@ -79,21 +79,21 @@ bash serverspeeder.sh;
 fi
 
 #锐速卸载程序
-if test $os == 7 ;then
+if test $os == 6 ;then
 echo '******正在卸载锐速程序，请等待几分钟******';
 chattr -i /serverspeeder/etc/apx* && /serverspeeder/bin/serverSpeeder.sh uninstall -f;
 echo -e '卸载完成：			  [\033[32m  OK  \033[0m]';
 fi
 
 #多端口开放
-if test $os == 8 ;then
+if test $os == 7 ;then
 echo '******正在开放服务器端口，请等待几分钟******';
 wget -N --no-check-certificate ${url}/dk.sh;
 bash dk;
 fi
 
 #caddy安装
-if test $os == 9 ;then
+if test $os == 8 ;then
 echo '******正在下载caddy，请等待几分钟******';
 wget -N --no-check-certificate ${url}/caddy.sh;
 chmod +x caddy.sh;
@@ -103,7 +103,7 @@ bash caddy.sh;
 fi
 
 #一键安装ariaNG和filemanger
-if test $os == 10 ;then
+if test $os == 9 ;then
 echo '******正在安装ariaNG，请等待几分钟******';
 wget -N --no-check-certificate ${url}/ariaNGandfilemanger.sh;
 chmod +x ariaNGandfilemanger.sh;
@@ -111,7 +111,7 @@ bash ariaNGandfilemanger.sh;
 fi
 
 #一键安装libsodium
-if test $os == 11 ;then
+if test $os == 10 ;then
 echo '******正在安装libsodium，请等待几分钟******';
 wget -N --no-check-certificate ${url}/libsodium.sh;
 chmod +x libsodium.sh;
